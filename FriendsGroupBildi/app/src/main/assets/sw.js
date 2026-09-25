96d9fd64af6d23cbf4c15a5d77595c35d3a9992b
rakeshbbb500 <softwearenjiniear@gmail.com>
2026-09-25 23:54:17 +0530

    Create sw.js

---
+++ FriendsGroupBildi/app/src/main/assets/sw.js
@@ -0,0 +1,57 @@
+const CACHE_NAME = "friends-group-bildi-v1";
+
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./manifest.json",
+    "./logo.png"
+];
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(FILES_TO_CACHE);
+            })
+    );
+
+    self.skipWaiting();
+});
+
+
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+        caches.keys().then(keys => {
+
+            return Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
+
+        })
+    );
+
+    self.clients.claim();
+});
+
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+            .then(response => {
+
+                if (response) {
+                    return response;
+                }
+
+                return fetch(event.request);
+
+            })
+
+    );
+
+});
